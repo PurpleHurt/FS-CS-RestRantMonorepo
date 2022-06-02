b1dd50a6e5938901517e14e8c39153df8214f5ef
Christina <christina.hurt@purplewave.com>
2022-06-01 21:59:02 -0500

    added helper methods

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -8,6 +8,7 @@ function Navigation() {
 
     const { currentUser } = useContext(CurrentUser)
 
+  
     let loginActions = (
         <>
             <li style={{ float: 'right' }}>
@@ -22,6 +23,8 @@ function Navigation() {
             </li>
         </>
     )
+    
+  
 
     if (currentUser) {
         loginActions = (
@@ -30,6 +33,22 @@ function Navigation() {
             </li>
         )
     }
+    
+    let addPlaceButton = null
+    
+    if (currentUser?.role === 'admin') {
+        addPlaceButton = (
+            <li>
+                <a href="#" onClick={() => history.push("/places/new")}>
+                    Add Place
+                </a>
+            </li>
+        )
+    }
+    
+ 
+    
+
 
     return (
         <nav>
@@ -44,11 +63,8 @@ function Navigation() {
                         Places
                     </a>
                 </li>
-                <li>
-                    <a href="#" onClick={() => history.push("/places/new")}>
-                        Add Place
-                    </a>
-                </li>
+                
+                {addPlaceButton}
                 {loginActions}
             </ul>
         </nav>
